08f8e48a27f94b77f36fd8425c6412f250c586ba
AtomCserkesz <58794179+AtomCserkesz@users.noreply.github.com>
2020-09-27 19:45:39 +0200

    teszt2

---
+++ CsovezetekSzimulacio/src/planInput.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class planInput extends JFrame{
+    private JPanel mainPanel;
+    private JPanel topLeftPanel;
+    private JPanel kezdoDatumPanel;
+    private JPanel bottomLeftPanel;
+    private JPanel bottomRightPanel;
+    private JTextField hovaField;
+    private JTextField honnanField;
+    private JTextField mennyisegField;
+    private JComboBox anyagBox;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JTextField a500TextField;
+    private JTextField a50TextField;
+    private JComboBox comboBox3;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JPanel vegeDatumPanel;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton newPlanButton;
+    private JButton submitButton;
+
+
+}
