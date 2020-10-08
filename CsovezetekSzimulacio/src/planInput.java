b5dc71fbab09bebdb51302278f705954049970b5
JarAKeksz <raschekattila@tutanota.com>
2020-10-08 19:35:48 +0200

    Date and time inputs automagically start at current datetime

--- CsovezetekSzimulacio/src/planInput.java
+++ CsovezetekSzimulacio/src/planInput.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import javax.swing.event.ListDataListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -26,15 +28,15 @@ public class planInput extends JFrame{
     private JTextField csohosszTextField;
     private JTextField atmeroTextField;
     private JComboBox kezdNapComboBox;
-    private JTextField kezdOraTextField;
-    private JTextField kezdPercTextField;
+    private JSpinner kezdOraTextField;
+    private JSpinner kezdPercTextField;
     private JPanel vegeDatumPanel;
     private JTextField nevTextField;
     private JTextField azonTextField;
     private JButton clearButton;
     private JButton submitButton;
-    private JTextField vegOraTextField;
-    private JTextField vegPercTextField;
+    private JSpinner vegOraTextField;
+    private JSpinner vegPercTextField;
     private JComboBox vegEvComboBox;
     private JComboBox vegHonapComboBox;
     private JComboBox vegNapComboBox;
@@ -65,8 +67,10 @@ public class planInput extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 csohosszTextField.setText("500"); atmeroTextField.setText("50");
-                honnanField.setText(""); hovaField.setText(""); mennyisegField.setText(""); kezdOraTextField.setText(""); kezdPercTextField.setText("");
-                vegOraTextField.setText(""); vegPercTextField.setText(""); nevTextField.setText(""); azonTextField.setText("");
+                /*honnanField.setText(""); hovaField.setText(""); */mennyisegField.setText("");
+                kezdOraTextField.setValue(LocalTime.now().getHour()); kezdPercTextField.setValue(LocalTime.now().getMinute());
+                vegOraTextField.setValue(LocalTime.now().getHour()); vegPercTextField.setValue(LocalTime.now().getMinute());
+                // nevTextField.setText(""); azonTextField.setText("");
                 anyagComboBox.setSelectedIndex(0);
             }
         });
@@ -79,11 +83,11 @@ public class planInput extends JFrame{
                 String startDate = kezdEvComboBox.getSelectedItem().toString()+"-"
                         +monthStart+"-"
                         +kezdNapComboBox.getSelectedItem().toString()+" "
-                        +kezdOraTextField.getText()+":"+kezdPercTextField.getText();
+                        +kezdOraTextField.getValue().toString()+":"+kezdPercTextField.getValue().toString();
                 String endDate = vegEvComboBox.getSelectedItem().toString()+"-"
                         +monthEnd+"-"
                         +vegNapComboBox.getSelectedItem().toString()+" "
-                        +vegOraTextField.getText()+":"+vegPercTextField.getText();
+                        +vegOraTextField.getValue().toString()+":"+vegPercTextField.getValue().toString();
                 Date start = new Date();
                 Date end = new Date();
                 try {
@@ -105,7 +109,7 @@ public class planInput extends JFrame{
                             endDate,
                             operatorIDComboBox.getSelectedItem().toString()));
                     new DataBaseHandler().insertRecord("transportationplan", tmp);
-                }else showMessageDialog(null,"A vég dátum nem lehet előrébb mint a kezdődátum");
+                }else showMessageDialog(null,"A befejezési dátumnak későbbinek kell lennie a kezdődátumnál!");
             }
         });
         operatorIDComboBox.addActionListener(new ActionListener() {
@@ -117,11 +121,34 @@ public class planInput extends JFrame{
         });
     }
     private void setGUIatStart() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        int currentYear = localDateTime.getYear();
+        int currentHour = localDateTime.getHour();
+        int currentMinute = localDateTime.getMinute();
+
+        kezdEvComboBox.addItem(currentYear);
+        kezdEvComboBox.addItem(currentYear+1);
+        kezdEvComboBox.setSelectedItem(currentYear);
+        kezdHonapComboBox.setSelectedIndex(localDateTime.getMonth().getValue()-1);
+        kezdNapComboBox.setSelectedIndex(localDateTime.getDayOfMonth()-1);
+        kezdOraTextField.setModel(new SpinnerNumberModel(currentHour, 0, 23, 1));
+        kezdPercTextField.setModel(new SpinnerNumberModel(currentMinute, 0, 59, 1));
+
+        vegEvComboBox.addItem(currentYear);
+        vegEvComboBox.addItem(currentYear+1);
+        vegHonapComboBox.setSelectedIndex(localDateTime.getMonth().getValue()-1);
+        vegNapComboBox.setSelectedIndex(localDateTime.getDayOfMonth()-1);
+        vegOraTextField.setModel(new SpinnerNumberModel(currentHour, 0, 23, 1));
+        vegPercTextField.setModel(new SpinnerNumberModel(currentMinute, 0, 59, 1));
+
         LinkedHashMap<Integer,ArrayList<String>> result = new LinkedHashMap<>();
         result = new DataBaseHandler().readRecords("operator");
         for (Map.Entry<Integer,ArrayList<String>> entry : result.entrySet()){
             operatorIDComboBox.addItem(new ComboItem(entry.getValue().get(0)));
         }
+
+        startDepoComboBox.addItem(new ComboItem(""));
+        endDepoComboBox.addItem(new ComboItem(""));
         operatorNameLable.setText(result.entrySet().iterator().next().getValue().get(1));
         result = new DataBaseHandler().readRecords("depo");
         for (Map.Entry<Integer,ArrayList<String>> entry : result.entrySet()){
